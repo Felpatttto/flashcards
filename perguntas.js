c4408ab24811e8009fe46a29f5acec7f2e2ee333
Felpatttto <00001118751504sp@al.educacao.sp.gov.br>
2024-11-25 02:26:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'carros o filme',
+    'Quem é o melhor amigo de McQueen?',
+    'O melhor amigo de McQueen é o Mate'
+)
+
+criaCartao(
+    'carros o filme',
+    'Qual é o lema de Doc Hudson?',
+    'O lema de Doc Hudson é "A corrida é ganha na curva."'
+)
+
+criaCartao(
+    'carros o filme',
+    'Qual é o nome do protagonista do filme?',
+    'O nome do protagonista é Relampago McQueen'
+)
+
+criaCartao(
+    'carros o filme',
+    'O que McQueen aprende em Radiator Springs?',
+    'Ele aprende sobre a importância da amizade e da humildade'
+)
